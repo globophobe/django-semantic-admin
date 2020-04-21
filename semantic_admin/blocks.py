0aca7a0c377db28c84bef674b9856448b79102a5
Alex <globophobe@gmail.com>
2020-04-21 22:58:56 +0900

    wip

--- semantic_admin/blocks.py
+++ semantic_admin/blocks.py
@@ -2,6 +2,6 @@ from django_react_streamfield.blocks import ChoiceBlock
 
 
 class SemanticChoiceBlock(ChoiceBlock):
-    def render_form(self, value, prefix="", errors=None):
-        string = super().render_form(value, prefix=prefix, errors=errors)
+    def render_form(self, *args, **kwargs):
+        string = super().render_form(*args, **kwargs)
         return string + "<script>semanticDropdowns()</script>"

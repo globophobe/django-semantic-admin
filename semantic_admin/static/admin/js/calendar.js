6612cee1ad2b56f0fe771e5ca86ae4f917c652f6
Alex <globophobe@gmail.com>
2022-01-09 12:19:47 +0900

    :sparkles: support calendar i18n

---
+++ semantic_admin/static/admin/js/calendar.js
@@ -0,0 +1,56 @@
+/*global gettext, interpolate, ngettext*/
+'use strict';
+
+const days = [
+    'Sunday', 
+    'Monday', 
+    'Tuesday', 
+    'Wednesday', 
+    'Thursday', 
+    'Friday', 
+    'Saturday', 
+];
+
+const months = [
+    'January', 
+    'February', 
+    'March', 
+    'April', 
+    'May', 
+    'June', 
+    'July', 
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
+
+const calendarI18n = {
+    days: ['S', 'M', 'T', 'W', 'T', 'F', 'S'].map(function(oneLetterDay, index) { 
+        const day = days[index];
+        return pgettext(`one letter ${day}`, oneLetterDay) 
+    }),
+    months: months.map(function(month) { 
+        return gettext(month) 
+    }),
+    monthsShort: [
+        'Jan', 
+        'Feb', 
+        'Mar', 
+        'Apr', 
+        'May', 
+        'Jun', 
+        'Jul', 
+        'Aug', 
+        'Sep', 
+        'Oct', 
+        'Nov', 
+        'Dec'
+    ].map(function(monthShort, index) { 
+        const monthLong = months[index];
+        return pgettext(`abbrev. month ${monthLong}`, monthShort) 
+    }),
+    today: gettext('Today'),
+    now: gettext('Now')
+}

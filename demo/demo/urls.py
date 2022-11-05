b3e04fd5ffef0adb0adccf53e1c7a454095bf306
Alex <globophobe@gmail.com>
2022-11-05 10:57:28 +0900

    :art: user, pass placeholder

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -14,11 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView
 from django.urls import path
 from django.views.generic.base import RedirectView
 from django.views.i18n import JavaScriptCatalog
 
+from demo.forms import LoginForm
+
 urlpatterns = [
+    path(
+        "login/",
+        LoginView.as_view(authentication_form=LoginForm),
+    ),
     path(
         "password_change/",
         RedirectView.as_view(url="/"),

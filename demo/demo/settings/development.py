09d62a4a79c7455e0c0172332c3638f0de9819e7
Alex <globophobe@gmail.com>
2024-02-03 15:26:39 +0900

    :hammer: use django debug toolbar in dev mode

--- demo/demo/settings/development.py
+++ demo/demo/settings/development.py
@@ -2,3 +2,7 @@ from .base import *  # noqa
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+INSTALLED_APPS.append("debug_toolbar")  # noqa: F405
+MIDDLEWARE.append("debug_toolbar.middleware.DebugToolbarMiddleware")  # noqa: F405
+INTERNAL_IPS = ["127.0.0.1"]

c6636566d30bdf2301c4deeeeee6fae4585a2689
Alex <globophobe@gmail.com>
2024-08-31 13:41:56 +0900

    :necktie: update formfield overrides

--- semantic_admin/admin.py
+++ semantic_admin/admin.py
@@ -86,11 +86,10 @@ class SemanticBaseModelAdmin(BaseModelAdmin):
         super().__init__(*args, **kwargs)
         # Simply overwrite
         overrides = copy.deepcopy(SEMANTIC_FORMFIELD_FOR_DBFIELD_DEFAULTS)
-        for k, v in self.formfield_overrides.items():
-            overrides.setdefault(k, {}).update(v)
+        for k, v in overrides.items():
+            self.formfield_overrides.setdefault(k, {}).update(v)
         for k, v in original_formfield_overrides.items():
-            overrides.setdefault(k, {}).update(v)
-        self.formfield_overrides = overrides
+            self.formfield_overrides.setdefault(k, {}).update(v)
 
     def formfield_for_choice_field(
         self, db_field: str, request: HttpRequest, **kwargs

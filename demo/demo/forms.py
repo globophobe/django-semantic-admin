cf7d2b849f3da2463dc31214df683240355c04e1
Alex <globophobe@gmail.com>
2024-12-07 13:42:07 +0900

    :children_crossing: submit form when demo password link clicked

--- demo/demo/forms.py
+++ demo/demo/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.admin.forms import AdminAuthenticationForm
 from django.contrib.auth.forms import UsernameField
 from django.utils.html import format_html
+from django.views.decorators.debug import sensitive_variables
 
 try:
     from django.utils.translation import gettext_lazy as _  # Django >= 4
@@ -20,10 +21,20 @@ class LoginForm(AdminAuthenticationForm):
     }
 
     username = UsernameField(
-        widget=forms.TextInput(attrs={"placeholder": "admin", "autofocus": True})
+        widget=forms.TextInput(attrs={"placeholder": "admin", "autofocus": True}),
+        required=False,
     )
     password = forms.CharField(
         label=_("Password"),
         strip=False,
         widget=forms.PasswordInput(attrs={"placeholder": "semantic"}),
+        required=False,
     )
+
+    @sensitive_variables()
+    def clean(self) -> dict:
+        """Clean."""
+        cleaned_data = super().clean()
+        if not cleaned_data.get("username") or not cleaned_data.get("password"):
+            raise self.get_invalid_login_error()
+        return cleaned_data

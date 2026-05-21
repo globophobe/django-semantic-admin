30d6624c5a94d1517475cdc57e02721812d7e210
Alex <globophobe@gmail.com>
2026-05-22 08:22:05 +0900

    :sparkles: add semantic admin auth form

--- semantic_admin/auth/forms.py
+++ semantic_admin/auth/forms.py
@@ -1,4 +1,11 @@
+from django.contrib.admin.forms import AdminAuthenticationForm
 from django.contrib.auth.forms import AdminPasswordChangeForm
+
+
+class SemanticAdminAuthenticationForm(AdminAuthenticationForm):
+    """Semantic admin login form."""
+
+    use_required_attribute = False
 
 
 class SemanticAdminPasswordChangeForm(AdminPasswordChangeForm):

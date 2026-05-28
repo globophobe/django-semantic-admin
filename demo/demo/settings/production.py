35531d896326638407130727b58162ee334e6d45
Alex <globophobe@gmail.com>
2026-05-29 07:57:16 +0900

    :wrench: prod settings

--- demo/demo/settings/production.py
+++ demo/demo/settings/production.py
@@ -12,10 +12,16 @@ ALLOWED_HOSTS = ["semantic-admin.com"]
 
 USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SECURE_SSL_REDIRECT = os.getenv("SECURE_SSL_REDIRECT", "true").lower() == "true"
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = int(os.getenv("SECURE_HSTS_SECONDS", "31536000"))
 
 sentry_sdk.init(
     dsn=os.environ["SENTRY_DSN"],
     integrations=[DjangoIntegration()],
+    include_local_variables=False,
+    send_default_pii=False,
     # Less transactions
     traces_sample_rate=0.01,
 )

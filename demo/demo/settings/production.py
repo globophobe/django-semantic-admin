59f0bd60e1f13abdb8251776f79a095bf4859bbb
Alex <globophobe@gmail.com>
2026-05-29 22:27:52 +0900

    :lock: secure settings

--- demo/demo/settings/production.py
+++ demo/demo/settings/production.py
@@ -16,6 +16,8 @@ SECURE_SSL_REDIRECT = os.getenv("SECURE_SSL_REDIRECT", "true").lower() == "true"
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_HSTS_SECONDS = int(os.getenv("SECURE_HSTS_SECONDS", "31536000"))
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
 
 sentry_sdk.init(
     dsn=os.environ["SENTRY_DSN"],
